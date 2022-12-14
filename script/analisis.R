419004381385f26cf327a7c2da4370d2862003f4
Ergostats Analysis <cdiergostats@gmail.com>
2022-12-14 09:17:28 -0500

    Instrucciones del EDA

--- script/analisis.R
+++ script/analisis.R
@@ -23,7 +23,7 @@ ventas_df <- read_csv("data/ventas_comercio.txt")
 
 # Diccionario:
 
-ventas_df <- read_csv("data/diccionario_ventas_comercio.txt")
+diccionario_df <- read_csv("data/diccionario_ventas_comercio.txt")
 
 
 # Transformación a objeto de tipo date: -----------------------------------
@@ -38,10 +38,6 @@ ymd("2012-10-07")
 
 dmy("1/1/2021")
 
-# Otro formato de excel:
-
-my("Ene-2019")
-
 # Extraer partes de una fecha:
 
 fechas <- ymd(c("20120101","20150407","20180909"))
@@ -54,7 +50,20 @@ quarter(fechas,type = "year.quarter")
 
 #### Aplicación sobre nuestra tabla:
 
-    
+# Solución cuando tienes en ingles, solo para windows:
+
+Sys.setlocale("LC_TIME", "Spanish")
+
+# EDA: --------------------------------------------------------------------
+# 1. Calcular las ventas totales (mutate)
+# 2. La proporción de ventas gravadas sobre las ventas totales (en el mismo mutate)
+# 3. Resumenes anuales: (group_by por año)
+#    (summarise: ) 
+#    3.1. Ventas promedio 
+#    3.2. Ventas totales
+#    3.3. Ventas medianas
+#    3.4. Desviación estandar
+#    3.5. Otro
 
 
 # Objetos: ts() para series de tiempo -------------------------------------
